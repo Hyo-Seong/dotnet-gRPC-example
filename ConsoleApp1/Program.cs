887cd2726ae6b3c587c7fe7d69ace748bfc1a6fa
Hyo-Seong <gytjd090792@gmail.com>
2022-01-08 18:57:50 +0900

    client init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Net.Client;
+using gRPC_example;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5052");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(
+        new HelloRequest { Name = ".NET conf" }
+    );
+
+
+Console.WriteLine(reply.Message);
